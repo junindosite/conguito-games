e51ba771b0314aa6482074092eec2275ae677bce
junindosite <juarezjunin2005@gmail.com>
2025-06-04 16:32:01 -0300

    animação do jogadorr

--- conguito/src/index.js
+++ conguito/src/index.js
@@ -59,3 +59,30 @@ function create() {
     player.setCollideWorldBounds(true);
     player.body.setGravityY(300);
 }
+
+// Criar animações do jogador
+    this.anims.create({
+        key: 'left',
+        frames: this.anims.generateFrameNumbers('dude', { start: 0, end: 3 }),
+        frameRate: 10,
+        repeat: -1
+    });
+
+    this.anims.create({
+        key: 'turn',
+        frames: [{ key: 'dude', frame: 4 }],
+        frameRate: 20
+    });
+
+    this.anims.create({
+        key: 'right',
+        frames: this.anims.generateFrameNumbers('dude', { start: 5, end: 8 }),
+        frameRate: 10,
+        repeat: -1
+    });
+
+    // Configurar colisões do jogador
+    this.physics.add.collider(player, platforms);
+    
+    // Configurar controles
+    cursors = this.input.ke
