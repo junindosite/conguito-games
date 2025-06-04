8cec958ccc3404d1b1f3ce5675df5d7ae1afb07e
junindosite <juarezjunin2005@gmail.com>
2025-06-04 16:31:14 -0300

    variaveis globais

--- conguito/src/index.js
+++ conguito/src/index.js
@@ -31,3 +31,31 @@ function preload() {
         frameHeight: 48
     });
 }
+
+// Variáveis globais
+var platforms;
+var player;
+var cursors;
+var stars;
+var bombs;
+var score = 0;
+var scoreText;
+var gameOver = false;
+
+function create() {
+    // Adicionar fundo
+    this.add.image(400, 300, 'sky');
+
+    // Criar plataformas
+    platforms = this.physics.add.staticGroup();
+    platforms.create(400, 568, 'ground').setScale(2).refreshBody();
+    platforms.create(600, 400, 'ground');
+    platforms.create(50, 250, 'ground');
+    platforms.create(750, 220, 'ground');
+
+    // Criar jogador
+    player = this.physics.add.sprite(100, 450, 'dude');
+    player.setBounce(0.2);
+    player.setCollideWorldBounds(true);
+    player.body.setGravityY(300);
+}
